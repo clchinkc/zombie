ff63eea68cfe41e829729edbf4b953fc24427018
CHIN Chi Lon <clchin@connect.ust.hk>
2023-02-05 15:57:12 +0800

    modified:   .vscode/PythonImportHelper-v2-Completion.json 	deleted:    mazes.py 	modified:   pathfinder.py

--- pathfinder.py
+++ pathfinder.py
@@ -233,14 +233,13 @@ path_finding()
 
 """
 class maze:
-	def __init__(self,layout,exits=None,name='Maze',time=0,random=None,rand_up=False,end=None):
-		self.layout=layout
-		self.exits=exits
-		self.name=name.capitalize()
+	def __init__(self,layout,exits=None,name='Maze',time=0,random=None,end=None):
+		self.layout=layout # a list of lists of numbers
+		self.exits=exits # a list of the exits in the order of north, east, south, west
+		self.name=name.capitalize() # the name of the maze
 		self.time=time*60
-		self.random=random
-		self.up=rand_up
-		self.end=end
+		self.random=random # the size of the random maze
+		self.end=end # the end message
 	def get_maze(self):
 		if self.random:
 			return generate(self.random*4-1,self.up)
@@ -248,4 +247,6 @@ class maze:
 			return self.layout
 	def get_exits(self):
 		return self.exits
+
+# master_maze a list of all the mazes in the game and some places storing special items
 """
