4c609720d50abd00c8bd6e070f43de6d00b73e10
CHIN Chi Lon <clchin@connect.ust.hk>
2023-03-15 14:06:00 +0800

    modified:   stock_prediction_3.py 	modified:   stock_prediction_5.py

--- stock_prediction_3.py
+++ stock_prediction_3.py
@@ -1,100 +1,145 @@
 
 """
-There are several time series models that you can use to predict stock prices, 
-including ARIMA (Autoregressive Integrated Moving Average), SARIMA (Seasonal ARIMA), and Prophet. 
-Each model has its strengths and weaknesses, and you should choose the one that best fits your data.
+This code performs time-series analysis using KMeans clustering and Markov chain modeling to predict future stock prices. Here is an overview of what each section of the code does:
+
+Step 1: Data Preprocessing
+
+Reads in the stock data from a CSV file and drops any missing values.
+Scales the Close prices using the MinMaxScaler.
+Step 2: Defining the States
+
+Defines the number of states to use in the Markov chain model.
+Uses KMeans clustering to group the scaled Close prices into the specified number of clusters (i.e., states).
+Assigns each data point to its corresponding state.
+Step 3: Transition Probability Matrix
+
+Constructs a transition probability matrix based on the frequency of transitions between the states in the data.
+Normalizes the matrix so that each row sums to 1.
+Step 4: Predicting the Future States
+
+Calculates the probability of being in each state after one time step based on the current state probabilities and the transition probability matrix.
+Step 5: Predicting the Future Prices
+
+Calculates the estimated future price by taking a weighted average of the median prices of all the future states.
+Inversely scales the estimated future price to get the final prediction.
 """
+
+
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_squared_error
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.stattools import adfuller
+from sklearn.cluster import KMeans
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import MinMaxScaler
 
-# Step 1: Data Collection
-df = pd.read_csv('apple_stock_data.csv', index_col=0, parse_dates=True)
 
-# Step 2: Data Preprocessing
-# Check for missing values and outliers
-if df.isna().values.any():
-    df.plot()
+# Step 1: Data Preprocessing
+def preprocess_data(filepath):
+    # Read data from CSV file
+    stock_data = pd.read_csv(filepath)
+    # Drop any missing values
+    stock_data = stock_data.dropna()
+    # stock_data = stock_data[['Close']]
+    scaler = MinMaxScaler()
+    stock_data = scaler.fit_transform(stock_data[['Close']])
+    return stock_data, scaler
+
+# Step 2: Defining the States
+def get_states(stock_data, num_states):
+    # Use KMeans clustering to group the scaled Close prices into the specified number of clusters (i.e., states)
+    kmeans = KMeans(n_clusters=num_states, init='k-means++', n_init=1, max_iter=300).fit(stock_data)
+    states = kmeans.labels_
+    return states
+
+def markov_chain_prediction(stock_data, states):
+    # Step 3: Transition Probability Matrix
+    hist = np.histogram2d(states[:-1], states[1:], bins=len(set(states)))[0]
+    transition_prob_matrix = hist / hist.sum(axis=1, keepdims=True)
+
+    # Step 4: Future State Probabilities
+    current_state_prob = np.array([1/len(set(states))]*len(set(states)))
+    future_state_prob = np.dot(current_state_prob, transition_prob_matrix)
+
+    # Step 5: Predicting the Future Prices
+    # weighted average of the median prices of all the future states
+    future_prices = [np.median(stock_data[states == i]) for i in range(len(set(states)))]
+    estimated_future_price = np.dot(future_state_prob, future_prices)
+    estimated_future_price_2d = np.array([estimated_future_price]).reshape(-1, 1)
+    return estimated_future_price_2d
+
+def mlp_prediction(stock_data, states):
+    # Step 4: Train a neural network classifier
+    clf = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=1000, solver='adam', random_state=42, early_stopping=True)
+    clf.fit(stock_data[:-1].reshape(-1, 1), states[1:])
+
+    # Step 5: Predict the future states of the last data point
+    proba = clf.predict_proba(stock_data[-1].reshape(-1, 1))
+    # weighted average of the median prices of all the future states
+    future_prices = [np.median(stock_data[states == i]) for i in range(len(set(states)))]
+    estimated_future_price = np.dot(proba[-1], future_prices)
+    estimated_future_price_2d = np.array([estimated_future_price]).reshape(-1, 1)
+    return estimated_future_price_2d
+
+def get_predictions(prediction_method, stock_data, states, days=30):
+    # Predict the next 30 days using the Markov chain model
+    stock_prices = stock_data
+    predictions = []
+    for i in range(days):
+        # Make a new prediction for the next day
+        last_price = prediction_method(stock_prices, states)
+        stock_prices = np.append(stock_prices, last_price)[1:]
+        predictions.append(last_price)
+    return predictions
+
+def plot_predictions(scaler, stock_data, predictions):
+    # Plot the predicted prices along with the historical data
+    historical_prices = scaler.inverse_transform(stock_data)
+    predicted_price = scaler.inverse_transform(np.array(predictions).reshape(-1, 1))
+    dates = pd.read_csv('apple_stock_data.csv')['Date'].values[-len(historical_prices):]
+    dates = [datetime.strptime(date, '%Y-%m-%d %H:%M:%S%z').replace(tzinfo=None) for date in dates]
+    future_dates = pd.date_range(start=dates[-1], periods=30, freq='D')
+
+    plt.plot(dates, historical_prices, label='Historical Prices')
+    plt.plot(future_dates, predicted_price, label='Predicted Prices')
+    plt.xticks(rotation=45)
+    plt.legend()
     plt.show()
-# Check for missing values
-if df.isnull().values.any():
-    df = df.dropna()
+    
+def main():
+    stock_data, scaler = preprocess_data('apple_stock_data.csv')
+    states = get_states(stock_data, 5)
+    
+    # Predict the next days using the Markov chain model and the MLP model
+    predictions_markov = get_predictions(markov_chain_prediction, stock_data, states, days=30)
+    predictions_mlp = get_predictions(mlp_prediction, stock_data, states, days=30)
+    # get the average of the two predictions
+    predictions = (np.array(predictions_markov) + np.array(predictions_mlp)) / 2
+    
+    # Plot the predicted prices along with the historical data
+    plot_predictions(scaler, stock_data, predictions)
 
-# Step 3: Data Visualization
-plt.figure(figsize=(10,5))
-plt.plot(df['Close'])
-plt.title('Stock Price')
-plt.xlabel('Date')
-plt.ylabel('Price ($)')
-plt.show()
-
-# Step 4: Check for Stationarity
-result = adfuller(df['Close'])
-print(f'ADF Statistic: {result[0]}')
-print(f'p-value: {result[1]}')
-
-# If the data is not stationary, take first differences
-diff = df['Close'].diff().dropna()
-
-# Check for stationarity again
-result = adfuller(diff)
-print(f'ADF Statistic: {result[0]}')
-print(f'p-value: {result[1]}')
-
-# Step 5: Split the data into training and testing sets
-# using last 90 days as test data
-split_index = len(diff) - 90
-train_data, test_data = diff[:split_index], diff[split_index:]
+if __name__ == "__main__":
+    main()
 
 
-# Step 6: Determine the order of differencing
-plot_acf(train_data)
-plot_pacf(train_data)
-plt.show()
+"""
+Add volatility term:
+To add volatility term, you can use the volatility of the stock price to adjust the predicted price. For example, you can use the standard deviation of the stock price to estimate the volatility.
 
-# Because the autocorrelation plot shows there's a high correlation between the current and past values up to around 10 lags
-# the partial autocorrelation plot shows a significant lag at 3
-# Based on the ACF and PACF plots, choose an ARIMA(10, 1, 3) model
-# Step 7: Determine the order of the ARIMA model
-model = ARIMA(train_data, order=(10, 1, 3))
-# Perform parameter estimation
-model_fit = model.fit()
+Use a more sophisticated clustering algorithm:
+To use a more sophisticated clustering algorithm, you can try hierarchical clustering, density-based clustering, or model-based clustering algorithms and evaluate their performance.
 
-# Step 8: Evaluate the ARIMA model
-# Evaluate the model using MSE and RMSE
-predictions = model_fit.predict(start=len(train_data), end=len(train_data)+len(test_data)-1, typ='levels')
-error = mean_squared_error(test_data, predictions)
-print(f'RMSE: {np.sqrt(error)}')
-# Evaluate the model using AIC and BIC scores
-print(model_fit.summary())
+Use more advanced machine learning models:
+To use more advanced machine learning models, you can try neural networks, support vector machines, decision trees, and random forests and evaluate their performance. You can also use ensemble methods such as stacking or boosting to combine the predictions of multiple models.
 
-# Visualize the predictions and the actual values
-plt.figure(figsize=(10,5))
-plt.plot(test_data.index, test_data, color='blue', label='Actual')
-plt.plot(predictions.index, predictions, color='red', label='Predicted')
-plt.title('Stock Price Prediction')
-plt.xlabel('Date')
-plt.ylabel('Price ($)')
-plt.legend()
-plt.show()
+Consider more features:
+To consider more features, you can collect and preprocess additional data such as volume, news sentiment, technical indicators, and economic indicators. You can then use feature selection methods such as correlation analysis, mutual information, or LASSO regression to select the most relevant features for the model.
 
-# Predicted values should be set with the same index as the actual values
+Use a different time scale:
+To use a different time scale, you can adjust the time interval of the data used for training and testing the model. For example, you can use hourly or weekly data instead of daily data.
 
-# Step 9: Forecast future stock prices
-future_dates = pd.date_range(start='2022-03-08', end='2023-03-08')
-# Forecast the future values using the trained model
-future_values = model_fit.forecast(steps=len(future_dates), typ='levels')
-plt.plot(df['Close'].diff(), label='Actual')
-plt.plot(future_dates, future_values, color='red', label='Forecasted')
-plt.show()
-
-# ValueError: x and y must have same first dimension, but have shapes (366,) and (30,)
-# The error is because the future dates have a different shape than the actual values
-
-
-# Step 10: Invert the differencing to get the forecasted stock prices
+Evaluate and fine-tune the model:
+To evaluate and fine-tune the model, you can use cross-validation techniques such as k-fold cross-validation or time series cross-validation to estimate the performance of the model. You can also use hyperparameter tuning techniques such as grid search, random search, or Bayesian optimization to optimize the parameters of the model.
+"""
--- stock_prediction_5.py
+++ stock_prediction_5.py
@@ -1,135 +1,100 @@
 
 """
+There are several time series models that you can use to predict stock prices, 
+including ARIMA (Autoregressive Integrated Moving Average), SARIMA (Seasonal ARIMA), and Prophet. 
+Each model has its strengths and weaknesses, and you should choose the one that best fits your data.
+"""
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.metrics import mean_squared_error
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.stattools import adfuller
 
-# Step 1: Data Preprocessing
-stock_data = pd.read_csv('apple_stock_data.csv')
-# get close price
-prices = stock_data['Close'].values
+# Step 1: Data Collection
+df = pd.read_csv('apple_stock_data.csv', index_col=0, parse_dates=True)
 
-# Define the transition matrix for the Markov chain
-transition_matrix = np.array([[0.7, 0.3],
-                            [0.4, 0.6]])
+# Step 2: Data Preprocessing
+# Check for missing values and outliers
+if df.isna().values.any():
+    df.plot()
+    plt.show()
+# Check for missing values
+if df.isnull().values.any():
+    df = df.dropna()
 
-# Define the initial state distribution
-initial_state = np.array([0.5, 0.5])
+# Step 3: Data Visualization
+plt.figure(figsize=(10,5))
+plt.plot(df['Close'])
+plt.title('Stock Price')
+plt.xlabel('Date')
+plt.ylabel('Price ($)')
+plt.show()
 
-# Define the number of steps to predict
-num_steps = 5
+# Step 4: Check for Stationarity
+result = adfuller(df['Close'])
+print(f'ADF Statistic: {result[0]}')
+print(f'p-value: {result[1]}')
 
-# Define a function to predict future prices using the Markov chain
-def predict_stock_price(prices, transition_matrix, initial_state, num_steps):
-    current_state = initial_state
-    predicted_prices = []
-    for i in range(num_steps):
-        predicted_price = np.dot(current_state, prices)
-        predicted_prices.append(predicted_price)
-        current_state = np.dot(current_state, transition_matrix)
-    return predicted_prices
+# If the data is not stationary, take first differences
+diff = df['Close'].diff().dropna()
 
-# Call the function to predict the future stock prices
-predicted_prices = predict_stock_price(prices, transition_matrix, initial_state, num_steps)
+# Check for stationarity again
+result = adfuller(diff)
+print(f'ADF Statistic: {result[0]}')
+print(f'p-value: {result[1]}')
 
-# Print the predicted prices
-print(predicted_prices)
-"""
-
-import numpy as np
-import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import MinMaxScaler
-
-# Step 1: Data Preprocessing
-stock_data = pd.read_csv('apple_stock_data.csv')
-# Drop any missing values
-stock_data = stock_data.dropna()
-# stock_data = stock_data[['Close']]
-scaler = MinMaxScaler()
-stock_data = scaler.fit_transform(stock_data[['Close']])
-
-# Step 2: Defining the States
-num_states = 5
-kmeans = KMeans(n_clusters=num_states, n_init=10, max_iter=1000).fit(stock_data)
-states = kmeans.labels_
-
-# Step 3: Transition Probability Matrix
-transition_prob_matrix = np.zeros((num_states, num_states))
-for i in range(len(states)-1):
-    transition_prob_matrix[states[i]][states[i+1]] += 1
-transition_prob_matrix /= transition_prob_matrix.sum(axis=1)[:, np.newaxis]
-
-# Step 4: Predicting the Future States
-current_state_prob = np.array([1/num_states]*num_states)
-future_state_prob = np.dot(current_state_prob, transition_prob_matrix)
-
-# Step 5: Predicting the Future Prices
-# weighted average of the median prices of all the future states
-future_prices = [np.median(stock_data[states == i]) for i in range(num_states)]
-estimated_future_price = np.dot(future_state_prob, future_prices)
-# median of prices of the most probable future state
-# future_price = np.median(stock_data[states == np.argmax(future_state_prob)])
-# estimated_future_price = sum(np.dot(future_state_prob, future_price))
-estimated_future_price_2d = np.array([estimated_future_price]).reshape(1, -1)
-predicted_price = scaler.inverse_transform(estimated_future_price_2d)
-print(predicted_price[0][0])
+# Step 5: Split the data into training and testing sets
+# using last 90 days as test data
+split_index = len(diff) - 90
+train_data, test_data = diff[:split_index], diff[split_index:]
 
 
+# Step 6: Determine the order of differencing
+plot_acf(train_data)
+plot_pacf(train_data)
+plt.show()
 
-from sklearn.metrics import classification_report
-from sklearn.neural_network import MLPClassifier
+# Because the autocorrelation plot shows there's a high correlation between the current and past values up to around 10 lags
+# the partial autocorrelation plot shows a significant lag at 3
+# Based on the ACF and PACF plots, choose an ARIMA(10, 1, 3) model
+# Step 7: Determine the order of the ARIMA model
+model = ARIMA(train_data, order=(10, 1, 3))
+# Perform parameter estimation
+model_fit = model.fit()
 
-# Step 5: Predicting the Future Prices
-X_train = stock_data[:-1]
-y_train = states[:-1]
-X_test = stock_data[1:]
-y_test = states[1:]
+# Step 8: Evaluate the ARIMA model
+# Evaluate the model using MSE and RMSE
+predictions = model_fit.predict(start=len(train_data), end=len(train_data)+len(test_data)-1, typ='levels')
+error = mean_squared_error(test_data, predictions)
+print(f'RMSE: {np.sqrt(error)}')
+# Evaluate the model using AIC and BIC scores
+print(model_fit.summary())
 
-# Train a neural network classifier
-clf = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=1000)
-clf.fit(X_train, y_train)
+# Visualize the predictions and the actual values
+plt.figure(figsize=(10,5))
+plt.plot(test_data.index, test_data, color='blue', label='Actual')
+plt.plot(predictions.index, predictions, color='red', label='Predicted')
+plt.title('Stock Price Prediction')
+plt.xlabel('Date')
+plt.ylabel('Price ($)')
+plt.legend()
+plt.show()
 
-# Predict the future states
-y_pred = clf.predict(X_test)
+# Predicted values should be set with the same index as the actual values
 
-# Reverse the scaling of the input data
-X_test_scaled = scaler.inverse_transform(X_test)
+# Step 9: Forecast future stock prices
+future_dates = pd.date_range(start='2022-03-08', end='2023-03-08')
+# Forecast the future values using the trained model
+future_values = model_fit.forecast(steps=len(future_dates), typ='levels')
+plt.plot(df['Close'].diff(), label='Actual')
+plt.plot(future_dates, future_values, color='red', label='Forecasted')
+plt.show()
 
-# Reverse the scaling of the predicted states
-y_pred_scaled = scaler.inverse_transform(y_pred.reshape(-1, 1)).flatten()
-
-# Generate the predicted stock prices
-predictions = [X_test_scaled[0][0]]
-for i in range(1, len(X_test_scaled)):
-    if y_pred_scaled[i] == 1:
-        predictions.append(predictions[i-1] * (1 + volatility))
-        raise ValueError('The predicted state is 1')
-    elif y_pred_scaled[i] == 2:
-        predictions.append(predictions[i-1] * (1 - volatility))
-        raise ValueError('The predicted state is 2')
-    else:
-        predictions.append(np.median(X_test_scaled[y_pred == y_pred[i]]))
-        
-# Print the predictions for the next day
-print(predictions[-1])
-        
-# add volatility term
+# ValueError: x and y must have same first dimension, but have shapes (366,) and (30,)
+# The error is because the future dates have a different shape than the actual values
 
 
-
-"""
-Use a more sophisticated clustering algorithm:
-To use a more sophisticated clustering algorithm, you can try hierarchical clustering, density-based clustering, or model-based clustering algorithms and evaluate their performance.
-
-Use more advanced machine learning models:
-To use more advanced machine learning models, you can try neural networks, support vector machines, decision trees, and random forests and evaluate their performance. You can also use ensemble methods such as stacking or boosting to combine the predictions of multiple models.
-
-Consider more features:
-To consider more features, you can collect and preprocess additional data such as volume, news sentiment, technical indicators, and economic indicators. You can then use feature selection methods such as correlation analysis, mutual information, or LASSO regression to select the most relevant features for the model.
-
-Use a different time scale:
-To use a different time scale, you can adjust the time interval of the data used for training and testing the model. For example, you can use hourly or weekly data instead of daily data.
-
-Evaluate and fine-tune the model:
-To evaluate and fine-tune the model, you can use cross-validation techniques such as k-fold cross-validation or time series cross-validation to estimate the performance of the model. You can also use hyperparameter tuning techniques such as grid search, random search, or Bayesian optimization to optimize the parameters of the model.
-"""
+# Step 10: Invert the differencing to get the forecasted stock prices
