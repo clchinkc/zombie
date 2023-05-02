9e97216caa33b82f2543e3b8527d28b34cf967d2
CHIN Chi Lon <clchin@connect.ust.hk>
2023-05-02 11:00:46 +0800

    modified:   study_resources 	deleted:    value iteration.py 	modified:   zombie unused/markov decision process.py

--- zombie unused/markov decision process.py
+++ zombie unused/markov decision process.py
@@ -1,182 +1,221 @@
 
-"""
-The code is an implementation of two reinforcement learning algorithms: value iteration and policy iteration. It uses a 3x4 grid world as the environment and aims to find the optimal policy for navigating the environment.
-
-The code first defines a function printEnvironment that prints the current state of the environment in a formatted manner. The environment is represented as a 2D list of utilities (U) or actions (policy). The function prints the utilities or actions in a grid format with "|" and "-" characters to separate the cells.
-
-The code also defines two helper functions getU and calculateU. getU takes as input the current state (r, c), an action (0-3), and the current utilities (U) and returns the utility of the state reached by performing the given action. calculateU takes as input the current state (r, c), an action (0-3), and the current utilities (U) and calculates the utility of the state that would be reached by performing the given action.
-
-The main algorithms are implemented in two functions: valueIteration and policyIteration. valueIteration performs value iteration until the utilities converge. It iteratively updates the utilities using the Bellman update equation and stops when the error between the current and next utilities is less than a given threshold. getOptimalPolicy is then called to get the optimal policy based on the converged utilities.
-
-policyIteration performs policy iteration by repeatedly improving the current policy until it converges. It starts by initializing a random policy and then iteratively updates the utilities using the simplified Bellman update equation, and the policy using the action that maximizes the utility for each state. It stops when the policy is unchanged after an iteration.
-
-The main function main takes as input the initial utilities (U), the initial policy, and the chosen algorithm (either 'value_iteration' or 'policy_iteration'). It calls the corresponding algorithm function and prints the results.
-
-The code also sets up some constants and variables needed for the environment and reinforcement learning algorithms. These include the reward for non-terminal states, the discount factor, the maximum allowable error, the actions that can be taken, and the dimensions of the environment. A random policy is also initialized in policy.
-"""
-
-import random
+import numpy as np
 
 
-# Visualization
-def printEnvironment(arr, policy=False):
-    res = ""
-    for r in range(NUM_ROW):
-        res += "|"
-        for c in range(NUM_COL):
-            if r == c == 1:
-                val = "WALL"
-            elif r <= 1 and c == 3:
-                val = "+1" if r == 0 else "-1"
-            else:
-                if policy:
-                    val = ["Down", "Left", "Up", "Right"][arr[r][c]]
-                else:
-                    val = str(arr[r][c])
-            res += " " + val[:5].ljust(5) + " |" # format
-        res += "\n"
-    print(res)
+class Environment:
+    def __init__(self, grid):
+        self.grid = grid
+        self.n_rows = len(grid)
+        self.n_cols = len(grid[0])
+        self.transitions = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+        self.costs = {'S': 0, 'G': 0, 'X': 0, 'T': 1}
 
-# Get the utility of the state reached by performing the given action from the given state
-def getU(U, r, c, action):
-    dr, dc = ACTIONS[action]
-    newR, newC = r+dr, c+dc
-    if newR < 0 or newC < 0 or newR >= NUM_ROW or newC >= NUM_COL or (newR == newC == 1): # collide with the boundary or the wall
-        return U[r][c]
-    else:
-        return U[newR][newC]
+    def is_valid(self, row, col):
+        return 0 <= row < self.n_rows and 0 <= col < self.n_cols and self.grid[row][col] != 'X'
 
-# Calculate the utility of a state given an action
-def calculateU(U, r, c, action):
-    u = REWARD
-    u += 0.1 * DISCOUNT * getU(U, r, c, (action-1)%4)
-    u += 0.8 * DISCOUNT * getU(U, r, c, action)
-    u += 0.1 * DISCOUNT * getU(U, r, c, (action+1)%4)
-    return u
+    def get_next_state(self, state, action):
+        row, col = state
+        drow, dcol = self.transitions[action]
+        new_row, new_col = row + drow, col + dcol
+
+        if self.is_valid(new_row, new_col):
+            return (new_row, new_col)
+        else:
+            return state
+
+    def get_cost(self, state):
+        row, col = state
+        return self.costs[self.grid[row][col]]
+
+    def get_actions(self, state):
+        return list(self.transitions.keys())
 
 
-# Value iteration
+def compute_action_values(env, state: tuple[int, int], value_function: np.ndarray, gamma: float = 0.99) -> np.ndarray:
+    """Compute the action values for a state using the value function."""
+    if env.grid[state[0]][state[1]] in ('X', 'G'):
+        return np.array([])
 
-# Perform value iteration until the utilities converge
-def valueIteration(U):
-    print("During the value iteration:\n")
+    action_values = np.zeros(len(env.transitions))
+
+    for i, action in enumerate(env.transitions):
+        next_state = env.get_next_state(state, action)
+        next_reward = env.get_cost(next_state) + gamma * value_function[next_state]
+        action_values[i] = next_reward
+
+    return action_values
+
+def update_value_function(env, gamma: float, state: tuple[int, int], value_function: np.ndarray) -> float:
+    """Update the value of a state using the Bellman equation."""
+    current_value = value_function[state]
+    action_values = compute_action_values(env, state, value_function, gamma)
+    
+    if len(action_values) > 0:
+        value_function[state] = np.min(action_values)
+
+    return abs(current_value - value_function[state])
+
+def select_best_action(env, state: tuple[int, int], value_function: np.ndarray) -> str:
+    """Select the best action for a state using the value function."""
+    action_values = compute_action_values(env, state, value_function)
+    
+    if len(action_values) == 0:
+        return ""
+
+    return list(env.transitions.keys())[np.argmin(action_values)]
+
+
+def value_iteration(env, gamma: float=0.99, epsilon: float=1e-4) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Run the value iteration algorithm to find the optimal value function and policy.
+
+    Args:
+        env: The environment object that defines the MDP.
+        gamma: The discount factor for future rewards.
+        epsilon: The convergence threshold for the algorithm.
+
+    Returns:
+        A tuple (value_function, policy) of numpy arrays representing the optimal value function
+        and policy, respectively.
+    """
+    # Initialize the value function and policy arrays
+    value_function = np.zeros((env.n_rows, env.n_cols))
+    policy = np.empty((env.n_rows, env.n_cols), dtype='<U1')
+
     while True:
-        nextU = [[0, 0, 0, 1], [0, 0, 0, -1], [0, 0, 0, 0], [0, 0, 0, 0]]
-        error = 0
-        for r in range(NUM_ROW):
-            for c in range(NUM_COL):
-                if (r <= 1 and c == 3) or (r == c == 1):
-                    continue
-                nextU[r][c] = max([calculateU(U, r, c, action) for action in range(NUM_ACTIONS)]) # Bellman update
-                error = max(error, abs(nextU[r][c]-U[r][c]))
-        U = nextU
-        printEnvironment(U)
-        if error < MAX_ERROR * (1-DISCOUNT) / DISCOUNT:
-            break
-    return U
+        max_change = 0
 
-# Get the optimal policy from U
-def getOptimalPolicy(U):
-    policy = [[-1, -1, -1, -1] for i in range(NUM_ROW)]
-    for r in range(NUM_ROW):
-        for c in range(NUM_COL):
-            if (r <= 1 and c == 3) or (r == c == 1):
+        # Update the value function for each state
+        for row in range(env.n_rows):
+            for col in range(env.n_cols):
+                state = (row, col)
+
+                change = update_value_function(env, gamma, state, value_function)
+                max_change = max(max_change, change)
+
+                # Select the best action for the state
+                policy[state] = select_best_action(env, state, value_function)
+
+        if max_change < epsilon:
+            # The algorithm has converged, return the optimal value function and policy
+            return value_function, policy
+
+
+def policy_evaluation(env, policy, V, gamma=0.99, epsilon=1e-4):
+    while True:
+        delta = 0
+
+        for row in range(env.n_rows):
+            for col in range(env.n_cols):
+                state = (row, col)
+                if env.grid[row][col] in ('X', 'G'):
+                    continue
+
+                v = V[state]
+                action = policy[state]
+                next_state = env.get_next_state(state, action)
+                next_reward = env.get_cost(next_state) + gamma * V[next_state]
+
+                V[state] = next_reward
+                delta = max(delta, abs(v - V[state]))
+
+        if delta < epsilon:
+            break
+
+    return V
+
+def policy_improvement(env, policy, V, gamma=0.99):
+    stable = True
+
+    for row in range(env.n_rows):
+        for col in range(env.n_cols):
+            state = (row, col)
+            if env.grid[row][col] in ('X', 'G'):
                 continue
-            # Choose the action that maximizes the utility
-            maxAction, maxU = None, -float("inf")
-            for action in range(NUM_ACTIONS):
-                u = calculateU(U, r, c, action)
-                if u > maxU:
-                    maxAction, maxU = action, u
-            policy[r][c] = maxAction
-    return policy
 
+            old_action = policy[state]
+            values = []
 
+            for action in env.get_actions(state):
+                next_state = env.get_next_state(state, action)
+                next_reward = env.get_cost(next_state) + gamma * V[next_state]
+                values.append(next_reward)
 
+            policy[state] = env.get_actions(state)[np.argmin(values)]
 
-# Policy iteration
+            if old_action != policy[state]:
+                stable = False
 
-# Perform some simplified value iteration steps to get an approximation of the utilities
-def policyEvaluation(policy, U):
+    return stable, policy
+
+def policy_iteration(env, gamma=0.99, epsilon=1e-4):
+    V = np.zeros((env.n_rows, env.n_cols))
+    policy = np.random.choice(list(env.transitions.keys()), (env.n_rows, env.n_cols))
+    policy = np.array([['' if env.grid[row][col] in ('X', 'G') else policy[row, col] for col in range(env.n_cols)] for row in range(env.n_rows)])
+
     while True:
-        nextU = [[0, 0, 0, 1], [0, 0, 0, -1], [0, 0, 0, 0], [0, 0, 0, 0]]
-        error = 0
-        for r in range(NUM_ROW):
-            for c in range(NUM_COL):
-                if (r <= 1 and c == 3) or (r == c == 1):
-                    continue
-                nextU[r][c] = calculateU(U, r, c, policy[r][c]) # simplified Bellman update
-                error = max(error, abs(nextU[r][c]-U[r][c]))
-        U = nextU
-        if error < MAX_ERROR * (1-DISCOUNT) / DISCOUNT:
+        V = policy_evaluation(env, policy, V, gamma, epsilon)
+        stable, policy = policy_improvement(env, policy, V, gamma)
+
+        if stable:
             break
-    return U
 
-def policyIteration(policy, U):
-    print("During the policy iteration:\n")
-    while True:
-        U = policyEvaluation(policy, U)
-        unchanged = True
-        for r in range(NUM_ROW):
-            for c in range(NUM_COL):
-                if (r <= 1 and c == 3) or (r == c == 1):
-                    continue
-                maxAction, maxU = None, -float("inf")
-                for action in range(NUM_ACTIONS):
-                    u = calculateU(U, r, c, action)
-                    if u > maxU:
-                        maxAction, maxU = action, u
-                if maxU > calculateU(U, r, c, policy[r][c]):
-                    policy[r][c] = maxAction # the action that maximizes the utility
-                    unchanged = False
-        if unchanged:
-            break
-        printEnvironment(policy)
-    return policy
-
-def main(U, policy, method='value_iteration'):
-    # Print the initial environment
-    print("The initial U is:\n")
-    printEnvironment(U)
-
-    if method == 'value_iteration':
-        # Value iteration
-        U = valueIteration(U)
-
-        # Get the optimal policy from U and print it
-        policy = getOptimalPolicy(U)
-        print("The optimal policy is:\n")
-        printEnvironment(policy, True)
-    elif method == 'policy_iteration':
-        # Print the initial random policy
-        print("The initial random policy is:\n")
-        printEnvironment(policy, True)
-
-        # Policy iteration
-        policy = policyIteration(policy, U)
-
-        # Print the optimal policy
-        print("The optimal policy is:\n")
-        printEnvironment(policy, True)
-    else:
-        print("Invalid method. Choose either 'value_iteration' or 'policy_iteration'.")
+    return V, policy
 
 
-if __name__ == '__main__':
+
+def print_map(env):
+    color_map = {'S': '\033[92mS\033[0m', 'G': '\033[94mG\033[0m', 'X': '\033[91mX\033[0m', 'T': '\033[93mT\033[0m'}
     
-    # Arguments
-    REWARD = -0.01 # constant reward for non-terminal states
-    DISCOUNT = 0.99
-    MAX_ERROR = 10**(-3)
+    for row in env.grid:
+        print(' '.join([color_map[cell] for cell in row]))
 
-    # Set up the initial environment
-    NUM_ACTIONS = 4
-    ACTIONS = [(1, 0), (0, -1), (-1, 0), (0, 1)] # Down, Left, Up, Right
-    NUM_ROW = 3
-    NUM_COL = 4
-    U = [[0, 0, 0, 1], [0, 0, 0, -1], [0, 0, 0, 0], [0, 0, 0, 0]]
-    policy = [[random.randint(0, 3) for j in range(NUM_COL)] for i in range(NUM_ROW)] # construct a random policy
-    
-    method = 'value_iteration'
-    # method = 'policy_iteration'
-    main(U, policy, method)
+def printV(V):
+    for row in range(V.shape[0]):
+        for col in range(V.shape[1]):
+            print(f"{V[row, col]:.2f}", end=' ')
+        print()
+
+def printPolicy(policy):
+    for row in range(policy.shape[0]):
+        for col in range(policy.shape[1]):
+            if policy[row, col] == 'U':
+                print('\u2191', end=' ')
+            elif policy[row, col] == 'D':
+                print('\u2193', end=' ')
+            elif policy[row, col] == 'L':
+                print('\u2190', end=' ')
+            elif policy[row, col] == 'R':
+                print('\u2192', end=' ')
+            else:
+                print('-', end=' ')
+        print()
+
+grid = [
+    ['S', 'T', 'X', 'T', 'G'],
+    ['T', 'X', 'T', 'T', 'T'],
+    ['T', 'T', 'T', 'X', 'T'],
+    ['X', 'T', 'X', 'T', 'T'],
+    ['T', 'T', 'T', 'T', 'T']
+]
+
+env = Environment(grid)
+print_map(env)
+
+
+# Testing the value iteration
+V_value_iter, policy_value_iter = value_iteration(env)
+print("\nState Values (Value Iteration):")
+printV(V_value_iter)
+
+print("\nOptimal Policy (Value Iteration):")
+printPolicy(policy_value_iter)
+
+
+# Testing the policy iteration
+# V_policy_iter, policy_policy_iter = policy_iteration(env)
+# print("\nState Values (Policy Iteration):")
+# printV(V_policy_iter)
+
+# print("\nOptimal Policy (Policy Iteration):")
+# printPolicy(policy_policy_iter)
