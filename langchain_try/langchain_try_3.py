9693b0ed9acb685256eec6750dd66e8986655618
CHIN Chi Lon <clchin@connect.ust.hk>
2023-06-20 23:46:40 +0800

    modified:   langchain_try/langchain_try_3.py

--- langchain_try/langchain_try_3.py
+++ langchain_try/langchain_try_3.py
@@ -1,5 +1,4 @@
 
-# https://huggingface.co/spaces/JavaFXpert/Chat-GPT-LangChain/blob/main/app.py
 
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
@@ -47,6 +46,7 @@ db = FAISS.from_documents(texts, embeddings)
 retriever = db.as_retriever(search_type="mmr", search_kwargs={"k": 1}) # Maximum Marginal Relevance Retrieval, specifying top k documents to return
 # retriever = db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .5}) # Similarity Score Threshold Retrieval, only returns documents with a score above that threshold
 
+
 splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=10, separator="。")
 redundant_filter = EmbeddingsRedundantFilter(embeddings=embeddings)
 llm_compressor = LLMChainExtractor.from_llm(ChatOpenAI(model="gpt-3.5-turbo"))
@@ -54,6 +54,7 @@ llm_compressor = LLMChainExtractor.from_llm(ChatOpenAI(model="gpt-3.5-turbo"))
 pipeline_compressor = DocumentCompressorPipeline(
     transformers=[splitter, redundant_filter]
 )
+
 
 compression_retriever = ContextualCompressionRetriever(base_compressor=pipeline_compressor, base_retriever=retriever)
 
@@ -74,19 +75,50 @@ human_prompt = PromptTemplate(
 )
 human_message_prompt = HumanMessagePromptTemplate(prompt=human_prompt)
 
+
 PROMPT = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 
+
 # If you want to use input_variables of the PromptTemplate, you can do so with the following:
-# chat_prompt.format_prompt(input_language="English", output_language="French", text="I love programming.").to_messages()
+# PROMPT.format_prompt(input_language="English", output_language="French", text="I love programming.").to_messages()
+
 
 qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0.1), chain_type="stuff", retriever=compression_retriever, chain_type_kwargs={"prompt": PROMPT}, return_source_documents=True)
+
+
+question_prompt_template = """Use the following portion of a long document to see if any of the text is relevant to answer the question. 
+Return any relevant text.
+{context}
+Question: {question}
+Relevant text, if any, in Traditional Chinese:"""
+QUESTION_PROMPT = PromptTemplate(
+    template=question_prompt_template, input_variables=["context", "question"]
+)
+
+combine_prompt_template = """Given the following extracted parts of a long document and a question, create a final answer in Traditional Chinese.
+
+QUESTION: {question}
+=========
+{summaries}
+=========
+Answer in Traditional Chinese:"""
+COMBINE_PROMPT = PromptTemplate(
+    template=combine_prompt_template, input_variables=["summaries", "question"]
+)
+
+
+# qa = RetrievalQA.from_chain_type(llm=OpenAI(model="text-davinci-003", temperature=0.), chain_type="map_reduce", retriever=compression_retriever, chain_type_kwargs={"question_prompt": QUESTION_PROMPT, "combine_prompt": COMBINE_PROMPT, "return_map_steps": False}, return_source_documents=True)
+
 
 # If you want to take a look at the documents in the database, you can do so with the following:
 # docs = retriever.get_relevant_documents("什麼是愛？")
 
+
 query = "什麼是愛？"
 
+
 returned_result = qa(query)
+
 
 print(returned_result['result'])
 print(returned_result['source_documents'])
