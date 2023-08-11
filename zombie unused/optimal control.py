479ad3f1a5b1bc6edc6bd7e8a0915551752bfc92
CHIN Chi Lon <clchin@connect.ust.hk>
2023-08-11 08:28:04 +0800

    modified:   zombie unused/optimal control.py 	deleted:    physics_engine.cpp 	deleted:    physics_engine.py 	deleted:    zombie unused/flocking.py 	deleted:    zombie unused/flocking_pygame.py

--- zombie unused/optimal control.py
+++ zombie unused/optimal control.py
@@ -1,4 +1,31 @@
 
+"""
+Using the algebraic Riccati equation (ARE) in a zombie apocalypse simulation might sound unconventional, but it's possible to frame aspects of the simulation in a way that leverages optimal control theory. Here's how you might go about it:
+
+### 1. **Modeling the System**:
+   - **State Variables**: Define the state of the system using variables such as the number of zombies, the number of healthy humans, resources, defense mechanisms, etc.
+   - **Control Inputs**: Define the control inputs that can be manipulated to affect the system's state. These might include decisions on resource allocation, human deployment, attacking or retreating strategies, etc.
+   - **Dynamics**: Create a mathematical model to describe how the state of the system evolves over time, considering the control inputs.
+
+### 2. **Defining the Objective**:
+   - **Objective Function**: Establish an objective function that you wish to optimize. This could be minimizing the number of zombies, maximizing the number of surviving humans, or a combination of several factors. A quadratic cost function could be employed, making the LQR approach relevant.
+   - **Constraints**: If there are constraints on the control inputs or state variables (like limited resources), these must also be included in the formulation.
+
+### 3. **Solving the Optimal Control Problem**:
+   - **Use LQR**: By casting the problem in the LQR framework, you can leverage the algebraic Riccati equation to find the optimal control law. This requires linearizing the system dynamics if they are nonlinear.
+   - **Simulate the Solution**: Implement the optimal control law in the simulation, allowing for adaptive strategies to respond to the zombie threat.
+
+### 4. **Adapting to a Nonlinear or Stochastic Scenario**:
+   - **Nonlinear Systems**: If the system's dynamics are highly nonlinear, you may need to look into more advanced control techniques like the Linear Quadratic Gaussian (LQG) or Model Predictive Control (MPC).
+   - **Uncertainty and Noise**: Zombie behavior could be unpredictable. If the system includes stochastic elements, the Kalman filter or its variants could be used for state estimation, incorporating the ARE.
+
+### 5. **Scenario Analysis**:
+   - **Different Strategies**: By altering the weights in the cost function or constraints, you can simulate different survival strategies and analyze how they perform under various scenarios.
+
+### Conclusion:
+While a zombie apocalypse scenario is fictional and highly complex, applying control theory concepts and the algebraic Riccati equation can still be a fascinating exercise. It would likely be an abstraction and simplification of the actual dynamics of such a scenario but could provide insights into strategic decision-making, resource allocation, and adaptive responses. It might also make for an engaging and educational project in the field of applied mathematics or systems engineering.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.linalg
@@ -48,3 +75,34 @@ plt.xlabel('Time')
 plt.ylabel('Population')
 plt.legend()
 plt.show()
+
+"""
+Simulating a zombie apocalypse is an unconventional application of control theory, and it involves modeling various aspects, such as human and zombie movement, human behavior, transmission dynamics, and so on. You might be interested in incorporating different techniques to create a realistic model. Here's a brief guide on how you can approach this task:
+
+1. **Modeling the Spread**:
+   - **SIR Models**: You can extend the Susceptible-Infected-Recovered (SIR) model to include zombies, treating the infection like a disease. You'll have Susceptible, Infected (zombies), and Recovered (or removed) states.
+   - **Agent-Based Models**: You can create individual agents that represent humans and zombies, each with its dynamics and rules. 
+
+2. **Control Techniques**:
+   - **Model Predictive Control (MPC)**: This would allow you to model the interaction dynamics and apply control inputs (like containment strategies, cure distribution, etc.) to minimize the spread.
+   - **Optimal Control with Dynamic Programming**: This method can help you find the optimal policy for various interventions over time.
+   - **Reinforcement Learning**: It's a more experimental approach where you could train an agent to control the spread of zombies using different strategies, rewarding scenarios where fewer people get infected.
+
+3. **Environment & Behavior Modeling**:
+   - **Game Theory**: Humans and zombies could be modeled as players in a game, each with their strategies and payoffs.
+   - **Network Models**: Social connections and physical locations can be represented as networks, affecting how the infection spreads.
+   - **Behavioral Models**: Modeling human panic, social behavior, and government interventions will add realism.
+
+4. **Simulation Tools**:
+   - **MATLAB/Simulink**: For traditional control methods.
+   - **Python with SciPy, NumPy, NetworkX**: For mathematical and network modeling.
+   - **Unity with ML-Agents**: If you want to create a more visual simulation with AI components.
+
+5. **Validation & Sensitivity Analysis**:
+   - You'd likely want to validate the model with real-world data (if possible) and perform sensitivity analysis to understand how various parameters affect the outcomes.
+
+6. **Ethical Considerations**:
+   - Ensure that the model's purpose and utilization are in line with ethical principles, particularly if it's being used for serious research or policy decisions.
+
+While this task doesn't fall within the conventional domain of optimal control theory, the synthesis of various modeling techniques, control methods, and simulation tools can make it an exciting and rewarding project.
+"""
