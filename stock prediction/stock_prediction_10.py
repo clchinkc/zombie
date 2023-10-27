19df8d21d26f547f39612b1a72a2500e3023e7e0
Kevin Chin <120100402+clchinkc@users.noreply.github.com>
2023-10-27 16:58:44 +0800

    modified:   stock prediction/stock_prediction_10.py

--- stock prediction/stock_prediction_10.py
+++ stock prediction/stock_prediction_10.py
@@ -15,74 +15,69 @@ initial_stock_price = 100
 expected_return = 0.05
 stock_volatility = 0.2
 time_step = 1
-investment_horizon = 1
 risk_free_rate = 0.03
-n_stock_price_states = 101
-stock_price_min = 50  # Minimum stock price in the discretized space
-stock_price_max = 150  # Maximum stock price in the discretized space
-n_simulation = 10000
+n_simulation = 1000
 n_actions = 101
 
-# Discretize stock price and action spaces
-stock_price_space = np.linspace(stock_price_min, stock_price_max, n_stock_price_states)
-action_space = np.linspace(0, 1, n_actions)  # Fractions representing percentage invested in stocks
+# Discretize action space: Fractions representing percentage invested in stocks
+action_space = np.linspace(0, 1, n_actions)
+stock_price_space = np.linspace(0, 2 * initial_stock_price, 2 * n_actions)
 
-# Initialize transition probabilities and rewards
-transition_probs = np.zeros((n_stock_price_states, len(action_space), n_stock_price_states))
-rewards = np.zeros((n_stock_price_states, len(action_space)))
+# Initialize rewards
+rewards = np.zeros((len(stock_price_space), len(action_space)))
+transition_probs = np.zeros((len(stock_price_space), len(action_space), len(stock_price_space)))
 
 np.random.seed(42)
-
-# Calculate returns based on a stochastic process
 simulated_returns = np.random.normal(expected_return * time_step, stock_volatility * np.sqrt(time_step), n_simulation)
 
-for current_price_idx, current_stock_price in enumerate(stock_price_space):
+for current_price_idx, current_price in enumerate(stock_price_space):
     for action_idx, fraction_invested in enumerate(action_space):
         combined_rewards = []
         
         for simulated_return in simulated_returns:
-            next_stock_price = current_stock_price * (1 + simulated_return)
-            idx_closest_price = np.argmin(np.abs(stock_price_space - next_stock_price))
-            transition_probs[current_price_idx, action_idx, idx_closest_price] += 1 / n_simulation
+            stock_return = fraction_invested * simulated_return
+            cash_return = (1 - fraction_invested) * risk_free_rate * time_step
+            total_return = stock_return + cash_return
+            combined_rewards.append(current_price * total_return)
+            
+            next_price = current_price * (1 + total_return)
+            idx_next_price = np.argmin(np.abs(stock_price_space - next_price))
+            transition_probs[current_price_idx, action_idx, idx_next_price] += 1 / n_simulation
+
+        rewards[current_price_idx, action_idx] = np.mean(combined_rewards)
 
 # Value iteration
-value_function = np.zeros(n_stock_price_states)
-max_iterations = 1000
+value_function = np.zeros(len(stock_price_space))
+max_iterations = 100
 convergence_tolerance = 1e-6
-discount_factor = 1
+discount_factor = np.exp(-risk_free_rate * time_step)
 
 for _ in range(max_iterations):
-    new_value_function = np.zeros(n_stock_price_states)
-    for i in range(n_stock_price_states):
-        new_value_function[i] = np.max(rewards[i] + discount_factor * np.dot(transition_probs[i], value_function))
+    new_value_function = np.zeros(len(stock_price_space))
+    for i in range(len(stock_price_space)):
+        action_values = rewards[i] + discount_factor * np.dot(transition_probs[i], value_function)
+        new_value_function[i] = np.max(action_values)
     if np.max(np.abs(value_function - new_value_function)) < convergence_tolerance:
         break
     value_function = new_value_function
 
-# Extract optimal policy
-optimal_policy = np.zeros(n_stock_price_states, dtype=float)
-for i in range(n_stock_price_states):
-    optimal_policy[i] = action_space[np.argmax(rewards[i] + discount_factor * np.dot(transition_probs[i], value_function))]
-
-# Simulate stock price path
+# Simulate stock price path and find the optimal investment fraction for each stock price
 T = 100
 stock_price_path = [initial_stock_price]
 optimal_fraction_path = []
 
-# Determine the optimal action for each stock price
-for idx, stock_price in enumerate(stock_price_path[:-1]):
-    idx_closest_price = np.argmin(np.abs(stock_price_space - stock_price))
-    optimal_fraction = optimal_policy[idx_closest_price]
-    optimal_fraction_path.append(optimal_fraction)
-
-    # Simulate next stock price based on the optimal action
+for t in range(T - 1):
     dt_return = np.random.normal(expected_return * time_step, stock_volatility * np.sqrt(time_step))
-    next_stock_price = stock_price * (1 + dt_return)
+    next_stock_price = stock_price_path[-1] * (1 + dt_return)
     stock_price_path.append(next_stock_price)
+
+    # Derive the optimal action for the current stock price
+    idx_closest_price = np.argmin(np.abs(stock_price_space - stock_price_path[-1]))
+    optimal_fraction = action_space[np.argmax(rewards[idx_closest_price] + discount_factor * np.dot(transition_probs[idx_closest_price], value_function))]
+    optimal_fraction_path.append(optimal_fraction)
 
 # Plotting
 fig, ax1 = plt.subplots(figsize=(10, 6))
-
 color = 'tab:red'
 ax1.set_xlabel('Time')
 ax1.set_ylabel('Stock Price', color=color)
