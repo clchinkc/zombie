29f8a4610ac4744762cc4eeccf7ca4d649df8eab
Kevin Chin <120100402+clchinkc@users.noreply.github.com>
2023-11-27 16:02:10 +0800

    modified:   missile_pid_1.py 	deleted:    missile_pid_2.py 	modified:   try.py

--- try.py
+++ try.py
@@ -1,150 +1,279 @@
-import heapq
+import sys
+import tkinter as tk
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from mpl_toolkits.mplot3d import Axes3D
 
 
-class Node:
-    def __init__(self, position):
-        self.position = np.array(position)
-        self.neighbors = []
-        self.g = float('inf')  # Cost to start node
-        self.rhs = float('inf')  # One-step lookahead cost
-        self.parent = None
+class PIDController:
+    def __init__(self, kp, ki, kd, control_limit=None):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.control_limit = control_limit
+        self.integral = 0
+        self.prev_error = None
 
-    def distance(self, other):
-        return np.linalg.norm(self.position - other.position)
+    def update(self, setpoint, measured_value, dt):
+        error = setpoint - measured_value
+        self.integral += error * dt
+        if self.prev_error is None:
+            derivative = 0
+        else:
+            derivative = (error - self.prev_error) / dt
+        self.prev_error = error
+        control_signal = self.kp * error + self.ki * self.integral + self.kd * derivative
+        if self.control_limit is not None:
+            control_signal = np.clip(control_signal, -self.control_limit, self.control_limit)
+        return control_signal
 
-    def __lt__(self, other):  # This will help with priority queue
-        return (self.rhs, self.g) < (other.rhs, other.g)
+class AdaptivePIDController:
+    def __init__(self, kp, ki, kd, kp_adapt, ki_adapt, kd_adapt, control_limit=None):
+        self.base_kp, self.base_ki, self.base_kd = kp, ki, kd
+        self.kp_adapt, self.ki_adapt, self.kd_adapt = kp_adapt, ki_adapt, kd_adapt
+        self.control_limit = control_limit
+        self.integral = self.prev_error = 0
 
-class Graph:
-    def __init__(self):
-        self.nodes = []
-        self.queue = []
-        self.start = None
-        self.goal = None
+    def update(self, setpoint, measured_value, dt):
+        error = setpoint - measured_value
+        self.integral += error * dt
+        derivative = (error - self.prev_error) / dt
 
-    def add_node(self, position):
-        node = Node(position)
-        self.nodes.append(node)
-        return node
+        # Adaptive tuning of PID parameters based on error magnitude
+        kp = self.base_kp + self.kp_adapt * np.abs(error)
+        ki = self.base_ki + self.ki_adapt * np.abs(error)
+        kd = self.base_kd + self.kd_adapt * np.abs(error)
 
-    def add_edge(self, node1, node2):
-        cost = node1.distance(node2)
-        node1.neighbors.append((node2, cost))
-        node2.neighbors.append((node1, cost))
+        control_signal = kp * error + ki * self.integral + kd * derivative
+        self.prev_error = error
 
-    def remove_node(self, node):
-        self.nodes.remove(node)
-        for neighbor, _ in node.neighbors:
-            neighbor.neighbors = [(n, c) for n, c in neighbor.neighbors if n != node]
+        if self.control_limit is not None:
+            control_signal = np.clip(control_signal, -self.control_limit, self.control_limit)
+        return control_signal
 
-    def update_vertex(self, node):
-        if node != self.goal:
-            # Find the neighbor with the minimum g + cost value
-            min_neighbor = min(node.neighbors, key=lambda n: n[0].g + n[1], default=(None, float('inf')))
-            node.rhs = min_neighbor[1] + min_neighbor[0].g if min_neighbor[0] is not None else float('inf')
-            node.parent = min_neighbor[0]  # Update parent to the node that provides the best path
-            
-        if node in self.queue:
-            self.queue.remove(node)  # Remove the node from the queue to update its priority
-            heapq.heapify(self.queue)
-        if node.g != node.rhs:
-            heapq.heappush(self.queue, node)  # Push it back with the updated values
+def missile_model(x, y, altitude, control_signals, dt, disturbance):
+    # Incorporating robust control by handling disturbances
+    x += control_signals['x'] * dt + disturbance
+    y += control_signals['y'] * dt + disturbance
+    altitude += control_signals['z'] * dt - 9.81 * dt + disturbance
+    return x, y, altitude
+
+def random_disturbance(strength=0.1):
+    return np.random.normal(0, strength)
+
+# Simulation Function
+# Global variable to track if the window is open
+window_open = True
+
+def on_window_close():
+    global window_open
+    window_open = False
+    window.destroy()
+
+def run_simulation():
+    global running, current_x, current_y, current_z, setpoint_x, setpoint_y, setpoint_z, window_open, x_data, y_data, z_data, time_points, velocities_x, velocities_y, velocities_z, accelerations, control_inputs_x, control_inputs_y, control_inputs_z, environmental_conditions
+
+    if running:
+        return  # Prevent multiple instances of the simulation
+
+    running = True
+    run_button.config(state=tk.DISABLED)  # Disable the run button to prevent re-clicks
+
+    # Simulation parameters
+    setpoint_z = 1000  # Desired altitude (meters)
+    setpoint_x = 100  # Desired x position (meters)
+    setpoint_y = 100  # Desired y position (meters)
+
+    current_x, current_y, current_z = 0, 0, 500  # Initial positions
+    x_data.clear()
+    y_data.clear()
+    z_data.clear()
+
+    pid_x = PIDController(kp=0.2, ki=0.02, kd=0.05, control_limit=50)
+    pid_y = PIDController(kp=0.2, ki=0.02, kd=0.05, control_limit=50)
+    pid_z = PIDController(kp=0.6, ki=0.1, kd=0.2, control_limit=100)  # Altitude control
+    # pid_x = AdaptivePIDController(kp=0.2, ki=0.02, kd=0.05, kp_adapt=0.005, ki_adapt=0.001, kd_adapt=0.005, control_limit=50)
+    # pid_y = AdaptivePIDController(kp=0.2, ki=0.02, kd=0.05, kp_adapt=0.005, ki_adapt=0.001, kd_adapt=0.005, control_limit=50)
+    # pid_z = AdaptivePIDController(kp=0.6, ki=0.1, kd=0.2, kp_adapt=0.01, ki_adapt=0.002, kd_adapt=0.01, control_limit=100)  # Altitude control
+
+    time_step = 0.1
+    n_steps = 500
+
+    # Define additional data structures for logging
+    velocities_x, velocities_y, velocities_z = [], [], []
+    accelerations = []
+    control_inputs_x, control_inputs_y, control_inputs_z = [], [], []
+    environmental_conditions = []
+
+    # Data for plotting
+    time_points = np.linspace(0, n_steps * time_step, n_steps)
+    x_positions, y_positions, z_positions = [], [], []
+    x_errors, y_errors, z_errors = [], [], []
+
+    # Initial states for velocity calculation
+    prev_x, prev_y, prev_altitude = current_x, current_y, current_z
+
+    # Simulation loop
+    for i in time_points:
+        disturbance = random_disturbance()
+        environmental_conditions.append(disturbance)
+
+        control_signal_x = pid_x.update(setpoint_x, current_x, time_step)
+        control_signal_y = pid_y.update(setpoint_y, current_y, time_step)
+        control_signal_z = pid_z.update(setpoint_z, current_z, time_step)
+
+        control_inputs_x.append(control_signal_x)
+        control_inputs_y.append(control_signal_y)
+        control_inputs_z.append(control_signal_z)
+
+        new_x, new_y, new_altitude = missile_model(
+            current_x, current_y, current_z,
+            {'x': control_signal_x, 'y': control_signal_y, 'z': control_signal_z},
+            time_step, disturbance
+        )
+
+        # Calculate velocity and acceleration
+        velocity_x = (new_x - prev_x) / time_step
+        velocity_y = (new_y - prev_y) / time_step
+        velocity_z = (new_altitude - prev_altitude) / time_step
+
+        velocities_x.append(velocity_x)
+        velocities_y.append(velocity_y)
+        velocities_z.append(velocity_z)
+
+        acceleration = (velocity_z - (prev_altitude - current_z) / time_step) / time_step
+        accelerations.append(acceleration)
+
+        # Update positions and errors
+        current_x, current_y, current_z = new_x, new_y, new_altitude
+        x_positions.append(current_x)
+        y_positions.append(current_y)
+        z_positions.append(current_z)
+
+        x_errors.append(setpoint_x - current_x)
+        y_errors.append(setpoint_y - current_y)
+        z_errors.append(setpoint_z - current_z)
+
+        # Prepare data for GUI update
+        data = {
+            'x_position': current_x,
+            'y_position': current_y,
+            'altitude': current_z,
+            'velocity_x': velocities_x[-1] if velocities_x else 0,
+            'velocity_y': velocities_y[-1] if velocities_y else 0,
+            'velocity_z': velocities_z[-1] if velocities_z else 0,
+            'control_x': control_inputs_x[-1] if control_inputs_x else 0,
+            'control_y': control_inputs_y[-1] if control_inputs_y else 0,
+            'control_z': control_inputs_z[-1] if control_inputs_z else 0,
+            'time': i * time_step
+        }
+
+        # Check if the window is still open before updating GUI
+        if not window_open:
+            break  # Break out of the loop if the window is closed
+
+        # Update GUI and Tkinter event processing
+        window.after(0, update_gui, data)
+        window.update_idletasks()
+        window.update()
+
+    running = False
+    if window_open:
+        run_button.config(state=tk.NORMAL)  # Re-enable the run button only if the window still exists
 
 
-    def compute_shortest_path(self):
-        while self.queue and (self.start.rhs != self.start.g or 
-                            any(node.g > node.rhs for node in self.queue)):
-            current = heapq.heappop(self.queue)
-            if current.g > current.rhs:
-                current.g = current.rhs
-                for neighbor, cost in current.neighbors:
-                    if neighbor.g > current.g + cost:  # Check if a better path is found
-                        neighbor.g = current.g + cost
-                        neighbor.parent = current  # Update parent pointer
-                        self.update_vertex(neighbor)
-            else:
-                current.g = float('inf')
-                for neighbor, cost in current.neighbors:
-                    if neighbor.parent == current:  # If the current node is the parent
-                        neighbor.rhs = min(neighbor.rhs, cost + self.compute_rhs(neighbor))
-                    self.update_vertex(neighbor)
-                self.update_vertex(current)
+def update_gui(data):
+    # Update labels with new data
+    position_label.config(text=f"Position: ({data['x_position']:.2f}, {data['y_position']:.2f}, {data['altitude']:.2f}) m")
+    velocity_label.config(text=f"Velocity: ({data['velocity_x']:.2f}, {data['velocity_y']:.2f}, {data['velocity_z']:.2f}) m/s")
+    control_label.config(text=f"Control Inputs: ({data['control_x']:.2f}, {data['control_y']:.2f}, {data['control_z']:.2f})")
+    time_label.config(text=f"Simulation Time: {data['time']:.2f} s")
 
-    def compute_rhs(self, node):
-        # This should return the rhs value based on the neighbors' g-values and costs.
-        # If the node is the goal, the rhs is by definition 0.
-        if node == self.goal:
-            return 0
-        return min(neighbor.g + cost for neighbor, cost in node.neighbors)
+    # Update the trajectory plot
+    x_data.append(data['x_position'])
+    y_data.append(data['y_position'])
+    z_data.append(data['altitude'])
+    ax_trajectory.clear()
+    ax_trajectory.plot(x_data, y_data, z_data, label='Missile Trajectory')
+    ax_trajectory.scatter(setpoint_x, setpoint_y, setpoint_z, color='r', marker='o', label='Target')
+    ax_trajectory.set_xlabel('X Position (m)')
+    ax_trajectory.set_ylabel('Y Position (m)')
+    ax_trajectory.set_zlabel('Altitude (m)')
+    ax_trajectory.set_title('Missile Trajectory')
+    ax_trajectory.legend()
 
+    # Update the position plot
+    ax_position.clear()
+    ax_position.plot(time_points[:len(x_data)], x_data, label='X Position')
+    ax_position.plot(time_points[:len(y_data)], y_data, label='Y Position')
+    ax_position.plot(time_points[:len(z_data)], z_data, label='Z Position')
+    ax_position.set_xlabel('Time (s)')
+    ax_position.set_ylabel('Position (m)')
+    ax_position.set_title('Position Over Time')
+    ax_position.legend()
 
-    def initialize(self, start, goal):
-        self.start = start
-        self.goal = goal
-        self.start.g = float('inf')
-        self.start.rhs = 0
-        self.goal.parent = None
-        heapq.heappush(self.queue, self.start)
-        self.compute_shortest_path()
+    # Update the velocity plot
+    ax_velocity.clear()
+    ax_velocity.plot(time_points[:len(velocities_x)], velocities_x, label='X Velocity')
+    ax_velocity.plot(time_points[:len(velocities_y)], velocities_y, label='Y Velocity')
+    ax_velocity.plot(time_points[:len(velocities_z)], velocities_z, label='Z Velocity')
+    ax_velocity.set_xlabel('Time (s)')
+    ax_velocity.set_ylabel('Velocity (m/s)')
+    ax_velocity.set_title('Velocity Over Time')
+    ax_velocity.legend()
 
-    def get_path(self):
-        # Reconstruct the path from goal to start
-        path = []
-        current = self.goal
-        while current is not None:
-            path.append(current)
-            current = current.parent
+    # Update the control signal plot
+    ax_control.clear()
+    ax_control.plot(time_points[:len(control_inputs_x)], control_inputs_x, label='X Control Signal')
+    ax_control.plot(time_points[:len(control_inputs_y)], control_inputs_y, label='Y Control Signal')
+    ax_control.plot(time_points[:len(control_inputs_z)], control_inputs_z, label='Z Control Signal')
+    ax_control.set_xlabel('Time (s)')
+    ax_control.set_ylabel('Control Signal')
+    ax_control.set_title('Control Signal Over Time')
+    ax_control.legend()
 
-        # Reverse the path so it goes from start to goal
-        return path[::-1]
+    canvas.draw()
+    fig.tight_layout()
 
-def visualize_graph(graph, start, goal, path=None, obstacle_nodes=None):
-    plt.figure(figsize=(10, 10))
-    for node in graph.nodes:
-        for neighbor, _ in node.neighbors:
-            plt.plot([node.position[0], neighbor.position[0]], [node.position[1], neighbor.position[1]], 'k-', lw=0.5)
-    plt.scatter([node.position[0] for node in graph.nodes if node not in obstacle_nodes], 
-                [node.position[1] for node in graph.nodes if node not in obstacle_nodes], c='blue')
-    if obstacle_nodes:
-        plt.scatter([node.position[0] for node in obstacle_nodes], 
-                    [node.position[1] for node in obstacle_nodes], c='black', marker='s', label='Obstacle')
-    plt.scatter([start.position[0]], [start.position[1]], c='green', marker='o', label='Start')
-    plt.scatter([goal.position[0]], [goal.position[1]], c='red', marker='x', label='Goal')
-    if path:
-        plt.plot([node.position[0] for node in path], [node.position[1] for node in path], 'r-', lw=2, label='Path')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+# Tkinter setup
+window = tk.Tk()
+window.title('Missile Trajectory Simulation')
+window.protocol("WM_DELETE_WINDOW", on_window_close)
 
-# Example usage of the Graph
-graph = Graph()
-node_spacing = 1
-for x in np.arange(0, 10, node_spacing):
-    for y in np.arange(0, 10, node_spacing):
-        graph.add_node((x, y))
+# Data for plotting
+x_data, y_data, z_data = [], [], []
 
-# Connect nodes in the graph
-for node1 in graph.nodes:
-    for node2 in graph.nodes:
-        if node1 != node2 and node1.distance(node2) <= 1.5 * node_spacing:
-            graph.add_edge(node1, node2)
+# Simulation running state
+running = False
 
-start = graph.nodes[0]  # Start at the first node
-goal = graph.nodes[-1]  # Goal is the last node
-graph.initialize(start, goal)
+# Start Simulation Button
+run_button = tk.Button(window, text="Start Simulation", command=run_simulation)
+run_button.pack()
 
-# Now add obstacles and update the graph as needed
-obstacle_nodes = []
-for node in graph.nodes:
-    if 3 <= node.position[0] <= 7 and 3 <= node.position[1] <= 7:
-        obstacle_nodes.append(node)
-        graph.remove_node(node)
-graph.initialize(start, goal)
+# Labels for data display
+position_label = tk.Label(window, text="Position: ")
+position_label.pack()
 
-# Visualization
-final_path = graph.get_path()
-print(f'Final path : {[node.position for node in final_path]}')
-visualize_graph(graph, start, goal, final_path, obstacle_nodes)
+velocity_label = tk.Label(window, text="Velocity: ")
+velocity_label.pack()
+
+control_label = tk.Label(window, text="Control Inputs: ")
+control_label.pack()
+
+time_label = tk.Label(window, text="Simulation Time: ")
+time_label.pack()
+
+# Matplotlib setup for plotting
+fig = Figure(figsize=(12, 8))
+ax_trajectory = fig.add_subplot(221, projection='3d')  # For 3D trajectory plot
+ax_position = fig.add_subplot(222)                    # For position plot
+ax_velocity = fig.add_subplot(223)                    # For velocity plot
+ax_control = fig.add_subplot(224)                     # For control signal plot
+canvas = FigureCanvasTkAgg(fig, master=window)
+widget = canvas.get_tk_widget()
+widget.pack()
+
+window.mainloop()
