7044e7e62951c8625bcb91780308d8294e41c976
Kevin Chin <120100402+clchinkc@users.noreply.github.com>
2024-02-14 14:34:11 +0800

    modified:   try.py 	modified:   try1.py 	deleted:    try3.py

--- try.py
+++ try.py
@@ -1,9 +1,7 @@
+import keras
 import numpy as np
 import tensorflow as tf
 from keras import layers
-from keras.constraints import MinMaxNorm
-from keras.models import Model
-from keras.optimizers import RMSprop
 
 
 # Define the generator
@@ -26,7 +24,7 @@ def build_generator(latent_dim, data_shape, num_classes, num_layers=1, filter_si
 
     x = layers.Conv2DTranspose(num_classes, kernel_size=(3, 3), padding='valid', activation="softmax")(x)
 
-    model = Model(inputs=[noise_input, timestep_input], outputs=x)
+    model = keras.models.Model(inputs=[noise_input, timestep_input], outputs=x)
     return model
 
 # Define the critic
@@ -43,78 +41,112 @@ def build_critic(data_shape, num_classes, num_layers=1, filter_size=32, dropout_
     x = layers.Dropout(dropout_rate)(x)
 
     for _ in range(num_layers):
-        x = layers.Conv2D(filter_size, kernel_size=(3, 3), padding='valid', kernel_constraint=MinMaxNorm(min_value=-0.01, max_value=0.01, rate=1.0))(x)
+        x = layers.Conv2D(filter_size, kernel_size=(3, 3), padding='valid')(x)
         x = layers.ELU()(x)
         x = layers.Dropout(dropout_rate)(x)
 
     x = layers.Flatten()(x)
     x = layers.Dense(1, activation='linear')(x)
 
-    model = Model(inputs=[data_input, timestep_input], outputs=x)
+    model = keras.models.Model(inputs=[data_input, timestep_input], outputs=x)
     return model
+
+# Define WGAN with Gradient Penalty
+def gradient_penalty(batch_size, real_images, fake_images, critic, labels, strength):
+    alpha = tf.random.uniform([batch_size, 1, 1, 1], minval=0, maxval=1)
+    interpolated_images = real_images * alpha + fake_images * (1 - alpha)
+    with tf.GradientTape() as tape:
+        tape.watch(interpolated_images)
+        prediction = critic([interpolated_images, labels], training=True)
+    gradients = tape.gradient(prediction, [interpolated_images])
+    gradients_norm = tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=[1, 2, 3]))
+    penalty = tf.reduce_mean((gradients_norm - 1.0) ** 2) * strength
+    return penalty
 
 # Update GAN building function
 def build_wgan(generator, critic):
-    # Wasserstein loss function
+    # Defining loss function inside due to TensorFlow's requirement for capturing in tf.function
     def wasserstein_loss(y_true, y_pred):
         return tf.reduce_mean(y_true * y_pred)
-    critic.compile(loss=wasserstein_loss, optimizer=RMSprop(learning_rate=0.00005))
+    
+    critic_optimizer = keras.optimizers.Nadam(learning_rate=0.00005)
+    generator_optimizer = keras.optimizers.Nadam(learning_rate=0.00005)
+    
+    critic.compile(loss=wasserstein_loss, optimizer=critic_optimizer)
     critic.trainable = False
 
     noise_input, label_input = generator.input
-    generated_image = generator.output
+    generated_image = generator([noise_input, label_input])
     critic_output = critic([generated_image, label_input])
+    gan = keras.models.Model([noise_input, label_input], critic_output)
+    gan.compile(loss=wasserstein_loss, optimizer=generator_optimizer)
 
-    gan = Model([noise_input, label_input], critic_output)
-    gan.compile(loss=wasserstein_loss, optimizer=RMSprop(learning_rate=0.00005))
+    return gan, critic, generator, critic_optimizer, generator_optimizer
 
-    return gan
 
-# Update training procedure
-def train_wgan(gan, generator, critic, latent_dim, epochs, batch_size, data_shape, num_classes, critic_interval=2, generator_interval=1):
-    valid = -np.random.uniform(low=0.9, high=1.0, size=batch_size)
-    fake = np.random.uniform(low=0.9, high=1.0, size=batch_size)
+@tf.function
+def critic_training_step(critic, generator, batch_size, latent_dim, num_classes, critic_optimizer, real_images, labels):
+    noise = tf.random.normal([batch_size, latent_dim])
+    fake_labels = tf.random.uniform([batch_size, 1], 0, num_classes, dtype=tf.int32)
+    
+    with tf.GradientTape() as tape:
+        fake_images = generator([noise, fake_labels], training=True)
+        real_output = critic([real_images, labels], training=True)
+        fake_output = critic([fake_images, fake_labels], training=True)
+        critic_real_loss = tf.reduce_mean(fake_output)
+        critic_fake_loss = -tf.reduce_mean(real_output)
+        gp = gradient_penalty(batch_size, real_images, fake_images, critic, labels, strength=10.0)
+        critic_loss = critic_real_loss + critic_fake_loss + gp
+    
+    critic_gradients = tape.gradient(critic_loss, critic.trainable_variables)
+    critic_optimizer.apply_gradients(zip(critic_gradients, critic.trainable_variables)) if critic_gradients else None
+    return critic_loss, critic_real_loss, critic_fake_loss
 
+@tf.function
+def generator_training_step(generator, critic, batch_size, latent_dim, num_classes, generator_optimizer):
+    noise = tf.random.normal([batch_size, latent_dim])
+    fake_labels = tf.random.uniform([batch_size, 1], 0, num_classes, dtype=tf.int32)
+    
+    with tf.GradientTape() as tape:
+        generated_images = generator([noise, fake_labels], training=True)
+        gen_output = critic([generated_images, fake_labels], training=True)
+        generator_loss = -tf.reduce_mean(gen_output)
+    
+    generator_gradients = tape.gradient(generator_loss, generator.trainable_variables)
+    generator_optimizer.apply_gradients(zip(generator_gradients, generator.trainable_variables)) if generator_gradients else None
+    return generator_loss
+
+def train_wgan(gan, generator, critic, critic_optimizer, generator_optimizer, data_shape, num_classes, latent_dim, epochs, batch_size, critic_interval, generator_interval):
     for epoch in range(epochs):
-        c_loss_real, c_loss_fake, g_loss_total = 0, 0, 0
-        
+        critic_losses, critic_real_losses, critic_fake_losses, generator_losses = [], [], [], []
         for _ in range(critic_interval):
-            noise = tf.random.normal([batch_size, latent_dim])
-            labels_noise = tf.random.uniform([batch_size, 1], minval=0, maxval=num_classes, dtype=tf.int32)
-            gen_imgs = generator.predict([noise, labels_noise], verbose=0)
-            labels_sample = tf.random.uniform([batch_size, 1], minval=0, maxval=num_classes, dtype=tf.int32)
-            real_imgs = tf.one_hot(tf.random.uniform([batch_size, *data_shape], minval=0, maxval=num_classes, dtype=tf.int32), depth=num_classes)
-
-            real_dataset = tf.data.Dataset.from_tensor_slices(((real_imgs, labels_sample), valid)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-            fake_dataset = tf.data.Dataset.from_tensor_slices(((gen_imgs, labels_noise), fake)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-
-            critic.trainable = True
-            c_real_loss = critic.fit(real_dataset, epochs=1, verbose=0)
-            c_fake_loss = critic.fit(fake_dataset, epochs=1, verbose=0)
-            c_loss_real += c_real_loss.history['loss'][-1]
-            c_loss_fake += c_fake_loss.history['loss'][-1]
-
-
+            real_labels = tf.random.uniform([batch_size, 1], 0, num_classes, dtype=tf.int32)
+            real_images = tf.one_hot(tf.random.uniform([batch_size, *data_shape], 0, num_classes, dtype=tf.int32), depth=num_classes)
+            critic_loss, critic_real_loss, critic_fake_loss = critic_training_step(
+                critic, generator, batch_size, latent_dim, num_classes, critic_optimizer, real_images, real_labels
+            )
+            critic_losses.append(critic_loss)
+            critic_real_losses.append(critic_real_loss)
+            critic_fake_losses.append(critic_fake_loss)
+            
         for _ in range(generator_interval):
-            critic.trainable = False
-            noise = tf.random.normal([batch_size, latent_dim])
-            labels_noise = tf.random.uniform([batch_size, 1], minval=0, maxval=num_classes, dtype=tf.int32)
-            noise_dataset = noise_dataset = tf.data.Dataset.from_tensor_slices(((noise, labels_noise), valid)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-            g_loss = gan.fit(noise_dataset, epochs=1, verbose=0)
-            g_loss_total += g_loss.history['loss'][-1]
-
-        c_loss_real_avg = c_loss_real / critic_interval
-        c_loss_fake_avg = c_loss_fake / critic_interval
-        g_loss_avg = g_loss_total / generator_interval
-
-        print(f"Epoch {epoch+1}/{epochs} [Critic: real loss: {c_loss_real_avg:.4f}, fake loss: {c_loss_fake_avg:.4f}] [Generator loss: {g_loss_avg:.4f}]")
+            generator_loss = generator_training_step(
+                generator, critic, batch_size, latent_dim, num_classes, generator_optimizer
+            )
+            generator_losses.append(generator_loss)
+        
+        avg_critic_loss = np.mean([loss.numpy() for loss in critic_losses])
+        avg_critic_real_loss = np.mean([loss.numpy() for loss in critic_real_losses])
+        avg_critic_fake_loss = np.mean([loss.numpy() for loss in critic_fake_losses])
+        avg_generator_loss = np.mean([loss.numpy() for loss in generator_losses])
+        print(f"Epoch {epoch + 1}/{epochs} \t[ Critic Loss: {avg_critic_loss:.4f}, Critic Real Loss: {avg_critic_real_loss:.4f}, Critic Fake Loss: {avg_critic_fake_loss:.4f}, Generator Loss: {avg_generator_loss:.4f} ]")
 
     sample_noise = np.random.normal(0, 1, (1, latent_dim))
-    sample_label = np.array([0]).reshape((1, 1))
-    generated_data = generator.predict([sample_noise, sample_label], verbose=0)
-    generated_data_class = np.argmax(generated_data, axis=-1).reshape(data_shape)
+    sample_labels = np.array([np.random.randint(0, num_classes)]).reshape((1, 1))
+    generated_images = generator.predict([sample_noise, sample_labels], verbose=0)
+    generated_class = np.argmax(generated_images, axis=-1).reshape(data_shape)
     print("Generated Data (class representation):")
-    print(generated_data_class)
+    print(generated_class)
 
 data_shape = (10, 10)
 latent_dim = np.prod(np.array(data_shape))
@@ -124,7 +156,9 @@ generator = build_generator(latent_dim, data_shape, num_classes)
 generator.summary()
 critic = build_critic(data_shape, num_classes)
 critic.summary()
-wgan = build_wgan(generator, critic)
+gan, critic, generator, critic_optimizer, generator_optimizer = build_wgan(generator, critic)
 
-train_wgan(wgan, generator, critic, latent_dim, epochs=20, batch_size=512, data_shape=data_shape, num_classes=num_classes,
-            critic_interval=3, generator_interval=1)
+# Train the model
+train_wgan(gan, generator, critic, critic_optimizer, generator_optimizer, data_shape, num_classes, latent_dim, epochs=20, batch_size=512, critic_interval=5, generator_interval=1)
+
+# Use tf.Data.dataset, fit after defining custom training loop
--- try1.py
+++ try1.py
@@ -1,164 +1,144 @@
-import keras
-import numpy as np
-import tensorflow as tf
-from keras import layers
+import hashlib
+import time
 
 
-# Define the generator
-def build_generator(latent_dim, data_shape, num_classes, num_layers=1, filter_size=16, dropout_rate=0.25):
-    noise_input = layers.Input(shape=(latent_dim,))
-    timestep_input = layers.Input(shape=(1,))
+class Vote:
+    def __init__(self, candidate):
+        self.candidate = candidate
 
-    timestep_dense = layers.Dense(latent_dim, use_bias=False)(timestep_input)
-    merged_input = layers.Add()([noise_input, timestep_dense])
+class Voter:
+    def __init__(self, voter_id_hash):
+        self.voter_id_hash = voter_id_hash
 
-    x = layers.Dense((data_shape[0] - 2 * num_layers - 2) * (data_shape[1] - 2 * num_layers - 2) * filter_size)(merged_input)
-    x = layers.Reshape((data_shape[0] - 2 * num_layers - 2, data_shape[1] - 2 * num_layers - 2, filter_size))(x)
-    x = layers.ELU()(x)
-    x = layers.Dropout(dropout_rate)(x)
+class Block:
+    def __init__(self, index, previous_hash, timestamp, votes, voter_ids, hash_):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.votes = votes
+        self.voter_ids = voter_ids
+        self.hash = hash_
 
-    for _ in range(num_layers):
-        x = layers.Conv2DTranspose(filter_size, kernel_size=(3, 3), padding='valid')(x)
-        x = layers.ELU()(x)
-        x = layers.Dropout(dropout_rate)(x)
+    def compute_hash(self):
+        block_data = (str(self.index) + str(self.previous_hash) +
+                      str(self.timestamp) + str([vote.__dict__ for vote in self.votes]) +
+                      str([voter.__dict__ for voter in self.voter_ids]))
+        return hashlib.sha256(block_data.encode('utf-8')).hexdigest()
 
-    x = layers.Conv2DTranspose(num_classes, kernel_size=(3, 3), padding='valid', activation="softmax")(x)
+    @staticmethod
+    def create_genesis_block():
+        return Block(0, '0', time.time(), [], [], Block.compute_hash(Block(0, '0', time.time(), [], [], '0')))
 
-    model = keras.models.Model(inputs=[noise_input, timestep_input], outputs=x)
-    return model
+def simple_encrypt_vote(vote_data, key):
+    return ''.join([chr(ord(vote_data[i]) + ord(key[i % len(key)])) for i in range(len(vote_data))])
 
-# Define the critic
-def build_critic(data_shape, num_classes, num_layers=1, filter_size=32, dropout_rate=0.25):
-    data_input = layers.Input(shape=(*data_shape, num_classes))
-    timestep_input = layers.Input(shape=(1,))
-    
-    timestep_dense = layers.Dense(int(np.prod(data_shape)) * num_classes, use_bias=False)(timestep_input)
-    timestep_reshaped = layers.Reshape((*data_shape, num_classes))(timestep_dense)
-    merged_input = layers.Add()([data_input, timestep_reshaped])
-    
-    x = layers.GaussianNoise(0.1)(merged_input)
-    x = layers.ELU()(x)
-    x = layers.Dropout(dropout_rate)(x)
+def simple_decrypt_vote(encrypted_vote, key):
+    return ''.join([chr(ord(encrypted_vote[i]) - ord(key[i % len(key)])) for i in range(len(encrypted_vote))])
 
-    for _ in range(num_layers):
-        x = layers.Conv2D(filter_size, kernel_size=(3, 3), padding='valid')(x)
-        x = layers.ELU()(x)
-        x = layers.Dropout(dropout_rate)(x)
+def validate_blockchain(blockchain):
+    for i in range(1, len(blockchain)):
+        current_block = blockchain[i]
+        previous_block = blockchain[i-1]
 
-    x = layers.Flatten()(x)
-    x = layers.Dense(1, activation='linear')(x)
+        if current_block.previous_hash != previous_block.hash:
+            return False
+        if current_block.hash != current_block.compute_hash():
+            return False
+    return True
 
-    model = keras.models.Model(inputs=[data_input, timestep_input], outputs=x)
-    return model
+def add_vote_to_blockchain(candidate, voter_id_hash, blockchain, encryption_key):
+    encrypted_vote = simple_encrypt_vote(candidate, encryption_key)
+    encrypted_voter_id = simple_encrypt_vote(voter_id_hash, encryption_key)
+    new_vote = Vote(encrypted_vote)
+    new_voter_id = Voter(encrypted_voter_id)
+    last_block = blockchain[-1]
+    new_block = Block(last_block.index + 1, last_block.hash, time.time(), [new_vote], [new_voter_id], '0')
+    new_block.hash = new_block.compute_hash()
 
-# Define WGAN with Gradient Penalty
-def gradient_penalty(batch_size, real_images, fake_images, critic, labels, strength):
-    alpha = tf.random.uniform([batch_size, 1, 1, 1], minval=0, maxval=1)
-    interpolated_images = real_images * alpha + fake_images * (1 - alpha)
-    with tf.GradientTape() as tape:
-        tape.watch(interpolated_images)
-        prediction = critic([interpolated_images, labels], training=True)
-    gradients = tape.gradient(prediction, [interpolated_images])
-    gradients_norm = tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=[1, 2, 3]))
-    penalty = tf.reduce_mean((gradients_norm - 1.0) ** 2) * strength
-    return penalty
+    if validate_blockchain(blockchain):
+        blockchain.append(new_block)
+        print(f"Block {new_block.index} added to the blockchain.")
+    else:
+        print("Failed to add block: Blockchain validation failed.")
 
-# Update GAN building function
-def build_wgan(generator, critic):
-    # Defining loss function inside due to TensorFlow's requirement for capturing in tf.function
-    def wasserstein_loss(y_true, y_pred):
-        return tf.reduce_mean(y_true * y_pred)
-    
-    critic_optimizer = keras.optimizers.Nadam(learning_rate=0.00005)
-    generator_optimizer = keras.optimizers.Nadam(learning_rate=0.00005)
-    
-    critic.compile(loss=wasserstein_loss, optimizer=critic_optimizer)
-    critic.trainable = False
+def cast_vote(voter_id, candidate, blockchain, encryption_key, allowed_candidates, voting_deadline, eligible_voter_hashes):
+    current_time = time.time()
+    if current_time > voting_deadline:
+        print("Voting failed: Voting deadline has passed.")
+        return
+    if candidate not in allowed_candidates:
+        print(f"Voting failed: '{candidate}' is not a valid candidate.")
+        return
+    voter_id_hash = hashlib.sha256(voter_id.encode('utf-8')).hexdigest()
+    if voter_id_hash not in eligible_voter_hashes:
+        print("Voting failed: Voter is not eligible.")
+        return
+    if any(voter.voter_id_hash == simple_encrypt_vote(voter_id_hash, encryption_key) for block in blockchain for voter in block.voter_ids):
+        print("Voting failed: Duplicate vote detected.")
+        return
+    add_vote_to_blockchain(candidate, voter_id_hash, blockchain, encryption_key)
+    print(f"Vote for '{candidate}' cast successfully.")
 
-    noise_input, label_input = generator.input
-    generated_image = generator([noise_input, label_input])
-    critic_output = critic([generated_image, label_input])
-    gan = keras.models.Model([noise_input, label_input], critic_output)
-    gan.compile(loss=wasserstein_loss, optimizer=generator_optimizer)
+def tally_votes(blockchain, decryption_key):
+    vote_count = {}
+    for block in blockchain:
+        for vote in block.votes:
+            decrypted_vote = simple_decrypt_vote(vote.candidate, decryption_key)
+            if decrypted_vote in vote_count:
+                vote_count[decrypted_vote] += 1
+            else:
+                vote_count[decrypted_vote] = 1
+    print("Final Vote Tally:")
+    for candidate, count in vote_count.items():
+        print(f"{candidate}: {count}")
+    return vote_count
 
-    return gan, critic, generator, critic_optimizer, generator_optimizer
+blockchain = [Block.create_genesis_block()]
+encryption_key = "secret_key"
+decryption_key = encryption_key
+allowed_candidates = ["CANDIDATE_A", "CANDIDATE_B"]
+eligible_voter_hashes = [
+    hashlib.sha256('VOTER_ID_1'.encode('utf-8')).hexdigest(),
+    hashlib.sha256('VOTER_ID_2'.encode('utf-8')).hexdigest(),
+]
+voting_deadline = time.time() + 10  # Extend the deadline for testing
 
+# 1. Successful Vote Casting
+print("Testing successful vote casting...")
+cast_vote('VOTER_ID_1', 'CANDIDATE_A', blockchain, encryption_key, allowed_candidates, voting_deadline, eligible_voter_hashes)
 
-@tf.function
-def critic_training_step(critic, generator, batch_size, latent_dim, num_classes, critic_optimizer, real_images, labels):
-    noise = tf.random.normal([batch_size, latent_dim])
-    fake_labels = tf.random.uniform([batch_size, 1], 0, num_classes, dtype=tf.int32)
-    
-    with tf.GradientTape() as tape:
-        fake_images = generator([noise, fake_labels], training=True)
-        real_output = critic([real_images, labels], training=True)
-        fake_output = critic([fake_images, fake_labels], training=True)
-        critic_real_loss = tf.reduce_mean(fake_output)
-        critic_fake_loss = -tf.reduce_mean(real_output)
-        gp = gradient_penalty(batch_size, real_images, fake_images, critic, labels, strength=10.0)
-        critic_loss = critic_real_loss + critic_fake_loss + gp
-    
-    critic_gradients = tape.gradient(critic_loss, critic.trainable_variables)
-    critic_optimizer.apply_gradients(zip(critic_gradients, critic.trainable_variables)) if critic_gradients else None
-    return critic_loss, critic_real_loss, critic_fake_loss
+# 2. Duplicate Voting Attempt
+print("Testing duplicate voting attempt...")
+cast_vote('VOTER_ID_1', 'CANDIDATE_B', blockchain, encryption_key, allowed_candidates, voting_deadline, eligible_voter_hashes)
 
-@tf.function
-def generator_training_step(generator, critic, batch_size, latent_dim, num_classes, generator_optimizer):
-    noise = tf.random.normal([batch_size, latent_dim])
-    fake_labels = tf.random.uniform([batch_size, 1], 0, num_classes, dtype=tf.int32)
-    
-    with tf.GradientTape() as tape:
-        generated_images = generator([noise, fake_labels], training=True)
-        gen_output = critic([generated_images, fake_labels], training=True)
-        generator_loss = -tf.reduce_mean(gen_output)
-    
-    generator_gradients = tape.gradient(generator_loss, generator.trainable_variables)
-    generator_optimizer.apply_gradients(zip(generator_gradients, generator.trainable_variables)) if generator_gradients else None
-    return generator_loss
+# 3. Invalid Candidate Voting Attempt
+print("Testing invalid candidate voting attempt...")
+cast_vote('VOTER_ID_2', 'INVALID_CANDIDATE', blockchain, encryption_key, allowed_candidates, voting_deadline, eligible_voter_hashes)
 
-def train_wgan(gan, generator, critic, critic_optimizer, generator_optimizer, data_shape, num_classes, latent_dim, epochs, batch_size, critic_interval, generator_interval):
-    for epoch in range(epochs):
-        critic_losses, critic_real_losses, critic_fake_losses, generator_losses = [], [], [], []
-        for _ in range(critic_interval):
-            real_labels = tf.random.uniform([batch_size, 1], 0, num_classes, dtype=tf.int32)
-            real_images = tf.one_hot(tf.random.uniform([batch_size, *data_shape], 0, num_classes, dtype=tf.int32), depth=num_classes)
-            critic_loss, critic_real_loss, critic_fake_loss = critic_training_step(
-                critic, generator, batch_size, latent_dim, num_classes, critic_optimizer, real_images, real_labels
-            )
-            critic_losses.append(critic_loss)
-            critic_real_losses.append(critic_real_loss)
-            critic_fake_losses.append(critic_fake_loss)
-            
-        for _ in range(generator_interval):
-            generator_loss = generator_training_step(
-                generator, critic, batch_size, latent_dim, num_classes, generator_optimizer
-            )
-            generator_losses.append(generator_loss)
-        
-        avg_critic_loss = np.mean([loss.numpy() for loss in critic_losses])
-        avg_critic_real_loss = np.mean([loss.numpy() for loss in critic_real_losses])
-        avg_critic_fake_loss = np.mean([loss.numpy() for loss in critic_fake_losses])
-        avg_generator_loss = np.mean([loss.numpy() for loss in generator_losses])
-        print(f"Epoch {epoch + 1}/{epochs} \t[ Critic Loss: {avg_critic_loss:.4f}, Critic Real Loss: {avg_critic_real_loss:.4f}, Critic Fake Loss: {avg_critic_fake_loss:.4f}, Generator Loss: {avg_generator_loss:.4f} ]")
+# 4. Ineligible Voter Voting Attempt
+print("Testing ineligible voter voting attempt...")
+cast_vote('VOTER_ID_3', 'CANDIDATE_A', blockchain, encryption_key, allowed_candidates, voting_deadline, eligible_voter_hashes)
 
-    sample_noise = np.random.normal(0, 1, (1, latent_dim))
-    sample_labels = np.array([np.random.randint(0, num_classes)]).reshape((1, 1))
-    generated_images = generator.predict([sample_noise, sample_labels], verbose=0)
-    generated_class = np.argmax(generated_images, axis=-1).reshape(data_shape)
-    print("Generated Data (class representation):")
-    print(generated_class)
+# 5. Late Voting Attempt
+print("Testing late voting attempt...")
+time.sleep(11)  # Delay to simulate voting after the deadline
+cast_vote('VOTER_ID_3', 'CANDIDATE_A', blockchain, encryption_key, allowed_candidates, voting_deadline, eligible_voter_hashes)
 
-data_shape = (10, 10)
-latent_dim = np.prod(np.array(data_shape))
-num_classes = 4
+# Wait a moment before tallying to ensure the late vote has been processed
+time.sleep(1)
 
-generator = build_generator(latent_dim, data_shape, num_classes)
-generator.summary()
-critic = build_critic(data_shape, num_classes)
-critic.summary()
-gan, critic, generator, critic_optimizer, generator_optimizer = build_wgan(generator, critic)
+# 6. Blockchain Integrity Check Post-Voting
+print("Checking blockchain integrity post-voting...")
+if validate_blockchain(blockchain):
+    print("Blockchain integrity check passed.")
+else:
+    print("Blockchain integrity check failed.")
 
-# Train the model
-train_wgan(gan, generator, critic, critic_optimizer, generator_optimizer, data_shape, num_classes, latent_dim, epochs=20, batch_size=512, critic_interval=5, generator_interval=1)
+# 7. Vote Tallying Accuracy
+print("Tallying votes for accuracy check...")
+tally_votes(blockchain, decryption_key)
 
-# Use tf.Data.dataset, fit after defining custom training loop
+# Print the blockchain data for inspection
+for block in blockchain:
+    print("Block:", block.index, "Hash:", block.hash, "Previous Hash:", block.previous_hash, "Votes:", [vote.__dict__ for vote in block.votes])
+
